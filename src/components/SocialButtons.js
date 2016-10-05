dc1ddbfb1a90483fd86604242b256b537f78fd5e
Guzman Monne <guzmonne@hotmail.com>
2016-10-05 16:26:13 -0300

    fiexed some style bugs

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Spinner from './Spinner.js'
 
 class SocialButtons extends React.Component {
 	constructor(props) {
 		super(props)
 		this.onClick = this.onClick.bind(this)
+		this.state = {provider: null}
 	}
 
 	onClick(provider, e) {
 		e.preventDefault()
+		this.setState({provider})
 		this.props.onClick(provider)
 	}
 
@@ -15,16 +18,31 @@ class SocialButtons extends React.Component {
 		return (
 			<div className="row">
 				<div className="col-xs-12">
-			    <a onClick={this.onClick.bind(this, 'facebook')}className="btn btn-social btn-facebook">
+				{this.state.provider === 'facebook' ? 
+					<a href="#" onClick={e => e.preventDefault()} className="btn btn-social btn-facebook">
+		        <i className="icon icon-facebook"></i>
+						<Spinner></Spinner>
+					</a>
+					:
+			    <a onClick={this.onClick.bind(this, 'facebook')} className="btn btn-social btn-facebook">
 		        <i className="icon icon-facebook"></i>
 		        Iniciar sesión con Facebook
 		      </a>
+				}
 				</div>
 				<div className="col-xs-12">
-		      <a onClick={this.onClick.bind(this, 'google')}className="btn btn-social btn-google">
+				{this.state.provider === 'google' ? 
+		      <a href="#" onClick={e => e.preventDefault()} className="btn btn-social btn-google">
+		        <i className="icon icon-google"></i>
+						<Spinner></Spinner>
+					</a>
+		      :
+		      <a onClick={this.onClick.bind(this, 'google')} className="btn btn-social btn-google">
 		        <i className="icon icon-google"></i>
 		        Iniciar sesión con Google
 		      </a>
+				} 
+
 				</div>
 			</div>
 		)
